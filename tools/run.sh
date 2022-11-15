e20c1b51b2da17f54fe76708a4d5e416a9102623
Dag König <dagk@microsoft.com>
2022-11-15 13:46:47 +0100

    update

---
+++ tools/run.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+info()
+{
+    echo '[INFO] ' "$@"
+}
+
+warn()
+{
+    echo '[WARN] ' "$@" >&2
+}
+
+fatal()
+{
+    echo '[ERROR] ' "$@" >&2
+    exit 1
+}
+
+SOURCE_REPO=https://github.com/buzzfrog/flux-helm-sops
+SOURCE_BRANCH=main
+KUSTOMIZE_FOLDER=./clusters/cluster1/app1
+
+# create cluster
+info "Create a new Kind cluster..."
+kind delete cluster 
+kind create cluster --config ./manifests/cluster-config.yaml
+
+flux install
+
+kubectl create namespace flux-workspace
+
+flux create source git source-repository \
+    --url=$SOURCE_REPO \
+    --secret-ref repository-auth \
+    --namespace flux-workspace \
+    --branch=main \
+    --interval=1m
+
+flux create kustomization cluster \
+    --path=.clusters/cluster1/app1 \
+    --source=source-repository \
+    --namespace flux-workspace \
+    --prune=true \
+    --interval=1m
